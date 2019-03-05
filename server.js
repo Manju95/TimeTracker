26bef1ed868a76a341bdfbad6ca0f3d2e37ebd58
Manjunath Shivanand Somawagol <SManjunath@quinnox.com>
2019-03-05 16:03:41 +0530

    update

--- server.js
+++ server.js
@@ -13,6 +13,14 @@ app.use(bodyparser.json({type:'*/*'}));
 app.use(bodyparser.urlencoded({extended:true}));
 app.use(cors());
 
+app.use(function(req, res, next) {  
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
+    res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization, Access-Control-Allow-Credentials");
+    res.setHeader("Access-Control-Allow-Credentials", "true");
+    next();
+  });
+
 // var time=firebase.database.ServerValue.TIMESTAMP;
 // console.log(time.valueOf());
 
