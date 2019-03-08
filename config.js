8aa830ce8932a3dd0fca9328e2a79793b6ae076c
Manjunath Shivanand Somawagol <SManjunath@quinnox.com>
2019-03-08 17:58:02 +0530

    after firebase addition

--- config.js
+++ config.js
@@ -1,10 +1,10 @@
-// var config = {
-//   apiKey: "AIzaSyDq9gUgW8jY0oGYukHFERIz_26TTdy0aXY",
-//   authDomain: "timetrack-3a7ba.firebaseapp.com",
-//   databaseURL: "https://timetrack-3a7ba.firebaseio.com",
-//   projectId: "timetrack-3a7ba",
-//   storageBucket: "timetrack-3a7ba.appspot.com",
-//   messagingSenderId: "121376587638"
-// };
+var config = {
+  apiKey: "AIzaSyDq9gUgW8jY0oGYukHFERIz_26TTdy0aXY",
+  authDomain: "timetrack-3a7ba.firebaseapp.com",
+  databaseURL: "https://timetrack-3a7ba.firebaseio.com",
+  projectId: "timetrack-3a7ba",
+  storageBucket: "timetrack-3a7ba.appspot.com",
+  messagingSenderId: "121376587638"
+};
 
-//   module.exports=config;
+  module.exports=config;
--- server.js
+++ server.js
@@ -1,10 +1,105 @@
 var express=require('express');
 var app=express();
+var config=require('./config');
+var firebase=require('firebase');
+var bodyparser=require('body-parser');
+var port=process.env.PORT || 3001;
+var cors=require('cors');
 
-var port=process.env.PORT || 3000;
+process.env["NODE_TLS_REJECT_UNAUTHORIZED"]=0;
 
-app.listen(port,()=>console.log(`server is listening at : ${port}`));
+app.listen(port,()=>console.log("server is running"));
+app.use(bodyparser.json({type:'*/*'}));
+app.use(bodyparser.urlencoded({extended:true}));
+app.use(cors());
 
-app.get('/getmsg',(req,res)=>{
-    res.send("Hey this is from server");
-})
+// var time=firebase.database.ServerValue.TIMESTAMP;
+// console.log(time.valueOf());
+
+app.get('/getmsg',(req,res)=>res.send("Hey server is running"));
+
+app.post('/person',(req,res)=>res.send(req.body.name));
+
+firebase.initializeApp(config);
+//firebase.database.enableLogging(true);
+
+app.get('/gettasks',(req,res)=>{
+var db=firebase.database().ref("Tasks");
+ db.once('value', (snap) => {
+            
+            res.json(snap.val());
+            })
+        .catch(error => {
+        res.json("eroror" . error)
+        });
+});
+
+app.post('/updatetask',(req,res)=>{
+
+    var endtime=req.body.endtime;
+    var userId=req.body.userid;
+    var duration=req.body.duration;
+    var uniqeId=req.body.uniqe;
+   
+    // var val=firebase.database().ref().child("Tasks");
+    // if(val.equalTo(userId)){
+    //     var db=firebase.database().ref().child("Tasks").child(''+userId+'/'+task);
+    // }else{
+    //     var db=firebase.database().ref().child("Tasks");
+    //     db=db.child(''+userId+'/'+task);
+    // }
+    var db=firebase.database().ref().child("Tasks");
+    db=db.child(''+userId+'/'+uniqeId);
+    
+    db.update({EndTime:endtime,Duration:duration},
+        function(error){
+            if(error){
+                res.send("Data not saved "+error);
+            }
+            else{
+                res.send("Data saved successfully");
+            }
+        }
+    );
+});
+
+app.get('/gettaskbyid/:id',(req,res)=>{
+    var id=req.params;
+    var db=firebase.database().ref().child("Tasks").child(''+id["id"]);
+    db.once('value',snap=>{
+        
+        res.json(snap.val());
+        
+    }).catch(error=>{
+        res.send("No record found "+error);
+    });
+});
+
+app.post('/savetask',(req,res)=>{
+    userId=req.body.userid
+    var obj={ 
+        Task:req.body.task,
+        StartTime:req.body.starttime,
+        EndTime:req.body.endtime,
+        Duration:req.body.duration,
+        CreatedDate:req.body.createddate
+    }
+    var val=firebase.database().ref().child("Tasks");
+    var db=val.child(''+userId);
+    
+    db.push(obj,
+        function(error){
+            // if(error){
+            //     res.send("Data not saved "+error);
+            // }
+            // else{
+            //     res.send("Data saved successfully");
+            // }
+        }
+    ).then(response => {
+         //res.send(JSON.stringify(response.getKey()));
+         res.send({ key: response.getKey() });
+        //console.log(response.getKey()) // this will return you ID
+    })
+    .catch(error => console.log(error));;
+});
