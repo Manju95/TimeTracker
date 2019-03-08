4ae2a260e4aae38b3fc8fb30a2d83abccc9fa702
Manjunath Shivanand Somawagol <SManjunath@quinnox.com>
2019-03-08 14:43:10 +0530

    updated after production dependancy

--- server.js
+++ server.js
@@ -25,83 +25,83 @@ app.post('/person',(req,res)=>res.send(req.body.name));
 //firebase.initializeApp(config);
 //firebase.database.enableLogging(true);
 
-app.get('/gettasks',(req,res)=>{
-var db=firebase.database().ref("Tasks");
- db.once('value', (snap) => {
+// app.get('/gettasks',(req,res)=>{
+// var db=firebase.database().ref("Tasks");
+//  db.once('value', (snap) => {
             
-            res.json(snap.val());
-            })
-        .catch(error => {
-        res.json("eroror" . error)
-        });
-});
+//             res.json(snap.val());
+//             })
+//         .catch(error => {
+//         res.json("eroror" . error)
+//         });
+// });
 
-app.post('/updatetask',(req,res)=>{
+// app.post('/updatetask',(req,res)=>{
 
-    var endtime=req.body.endtime;
-    var userId=req.body.userid;
-    var duration=req.body.duration;
-    var uniqeId=req.body.uniqe;
+//     var endtime=req.body.endtime;
+//     var userId=req.body.userid;
+//     var duration=req.body.duration;
+//     var uniqeId=req.body.uniqe;
    
-    // var val=firebase.database().ref().child("Tasks");
-    // if(val.equalTo(userId)){
-    //     var db=firebase.database().ref().child("Tasks").child(''+userId+'/'+task);
-    // }else{
-    //     var db=firebase.database().ref().child("Tasks");
-    //     db=db.child(''+userId+'/'+task);
-    // }
-    var db=firebase.database().ref().child("Tasks");
-    db=db.child(''+userId+'/'+uniqeId);
+//     // var val=firebase.database().ref().child("Tasks");
+//     // if(val.equalTo(userId)){
+//     //     var db=firebase.database().ref().child("Tasks").child(''+userId+'/'+task);
+//     // }else{
+//     //     var db=firebase.database().ref().child("Tasks");
+//     //     db=db.child(''+userId+'/'+task);
+//     // }
+//     var db=firebase.database().ref().child("Tasks");
+//     db=db.child(''+userId+'/'+uniqeId);
     
-    db.update({EndTime:endtime,Duration:duration},
-        function(error){
-            if(error){
-                res.send("Data not saved "+error);
-            }
-            else{
-                res.send("Data saved successfully");
-            }
-        }
-    );
-});
+//     db.update({EndTime:endtime,Duration:duration},
+//         function(error){
+//             if(error){
+//                 res.send("Data not saved "+error);
+//             }
+//             else{
+//                 res.send("Data saved successfully");
+//             }
+//         }
+//     );
+// });
 
-app.get('/gettaskbyid/:id',(req,res)=>{
-    var id=req.params;
-    var db=firebase.database().ref().child("Tasks").child(''+id["id"]);
-    db.once('value',snap=>{
+// app.get('/gettaskbyid/:id',(req,res)=>{
+//     var id=req.params;
+//     var db=firebase.database().ref().child("Tasks").child(''+id["id"]);
+//     db.once('value',snap=>{
         
-        res.json(snap.val());
+//         res.json(snap.val());
         
-    }).catch(error=>{
-        res.send("No record found "+error);
-    });
-});
+//     }).catch(error=>{
+//         res.send("No record found "+error);
+//     });
+// });
 
-app.post('/savetask',(req,res)=>{
-    var obj={ 
-        Task:req.body.task,
-        StartTime:req.body.starttime,
-        EndTime:req.body.endtime,
-        userId:req.body.userid,
-        Duration:req.body.duration,
-        CreatedDate:req.body.createddate
-    }
-    var val=firebase.database().ref().child("Tasks");
-    var db=val.child(''+obj.userId);
+// app.post('/savetask',(req,res)=>{
+//     var obj={ 
+//         Task:req.body.task,
+//         StartTime:req.body.starttime,
+//         EndTime:req.body.endtime,
+//         userId:req.body.userid,
+//         Duration:req.body.duration,
+//         CreatedDate:req.body.createddate
+//     }
+//     var val=firebase.database().ref().child("Tasks");
+//     var db=val.child(''+obj.userId);
     
-    db.push(obj,
-        function(error){
-            // if(error){
-            //     res.send("Data not saved "+error);
-            // }
-            // else{
-            //     res.send("Data saved successfully");
-            // }
-        }
-    ).then(response => {
-         //res.send(JSON.stringify(response.getKey()));
-         res.send({ key: response.getKey() });
-        //console.log(response.getKey()) // this will return you ID
-    })
-    .catch(error => console.log(error));;
-});
+//     db.push(obj,
+//         function(error){
+//             // if(error){
+//             //     res.send("Data not saved "+error);
+//             // }
+//             // else{
+//             //     res.send("Data saved successfully");
+//             // }
+//         }
+//     ).then(response => {
+//          //res.send(JSON.stringify(response.getKey()));
+//          res.send({ key: response.getKey() });
+//         //console.log(response.getKey()) // this will return you ID
+//     })
+//     .catch(error => console.log(error));;
+// });
